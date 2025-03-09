5ea73476e283a598eae4c3a653e5ba2884e29b5a
AlduinoCalderon <aldocalderon97@gmail.com>
2025-03-10 00:02:03 +0900

    feat:dark css style

--- js/main.js
+++ js/main.js
@@ -266,4 +266,21 @@ function initCustomCursor() {
 }
 
 // Uncomment the line below if you want to add a custom cursor
-// initCustomCursor();
+initCustomCursor();
+// Back to top button functionality
+const backToTopButton = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTopButton.classList.add('visible');
+  } else {
+    backToTopButton.classList.remove('visible');
+  }
+});
+
+backToTopButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
